2254052f988586a43dc40423c4723038f1305c0b
Alexander Fidel Marquez <alexanderfidelmarquez@gmail.com>
2019-03-30 11:24:31 -0400

    fix Proyect

--- version_2/prueba2.js
+++ version_2/prueba2.js
@@ -256,28 +256,3 @@ function mouseClik() {
         //console.log(x);
     });
 }
-/*function mouseClik(x) {
-    var ctx = document.getElementById('canvas1').getContext('2d');
-    ctx.canvas.addEventListener('click',function(event){
-        var mouseX = event.clientX - ctx.canvas.offsetLeft;
-        var mouseY = event.clientY - ctx.canvas.offsetTop;
-        var status = document.getElementById('status');
-        //console.log('x :' +mouseX + ' y : ' + mouseY);
-        x = [mouseX, mouseY];
-        //console.log(x);
-    });
-    console.log(x);
-    return x;
-   /!* ctx.canvas.addEventListener('click', function(event){
-        var mouseX = event.clientX - ctx.canvas.offsetLeft;
-        var mouseY = event.clientY - ctx.canvas.offsetTop;
-        // alert(mouseX+" | "+mouseY);
-        ctx.fillStyle = "orange";
-        ctx.fillRect(mouseX-15, mouseY-15, 30, 30);
-    });*!/
-}*/
-/*
-window.addEventListener('load', function(event) {
-    initCanvas();
-});
-*/
